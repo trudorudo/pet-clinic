b93b0b3447f2b7a1a6068bb213a2de1ab0959a6c
trudorudo <k.trudorudo@gmail.com>
2020-03-26 15:57:11 -0400

    addd PetTypeService

--- pet-clinic-data/src/main/java/guru/springboot/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springboot/petclinic/model/Owner.java
@@ -3,9 +3,40 @@ package guru.springboot.petclinic.model;
 import java.util.Set;
 
 public class Owner extends Person{
+    private String address;
+    private String city;
+    private String Telephone;
     private Set<Pet> pet;
 
-    public Owner(Set<Pet> pet) {
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getTelephone() {
+        return Telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        Telephone = telephone;
+    }
+
+    public Set<Pet> getPet() {
+        return pet;
+    }
+
+    public void setPet(Set<Pet> pet) {
         this.pet = pet;
     }
 }
---
+++ pet-clinic-data/src/main/java/guru/springboot/petclinic/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package guru.springboot.petclinic.services;
+
+import guru.springboot.petclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+}
