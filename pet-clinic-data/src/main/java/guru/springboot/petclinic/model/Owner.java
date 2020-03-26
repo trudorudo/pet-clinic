8ee9da8e29ffeb91eecd795d0cb70a2c6501e9d6
trudorudo <k.trudorudo@gmail.com>
2020-03-26 15:40:26 -0400

    add more entities

--- pet-clinic-data/src/main/java/guru/springboot/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springboot/petclinic/model/Owner.java
@@ -1,4 +1,11 @@
 package guru.springboot.petclinic.model;
 
+import java.util.Set;
+
 public class Owner extends Person{
+    private Set<Pet> pet;
+
+    public Owner(Set<Pet> pet) {
+        this.pet = pet;
+    }
 }
