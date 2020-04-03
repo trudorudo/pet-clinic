a97a2ef6c5a5f94a433dce1c6c5e0c4ce99d7207
trudorudo <k.trudorudo@gmail.com>
2020-04-03 13:41:33 -0400

    adding project Lombok

--- pet-clinic-data/src/main/java/guru/springboot/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/springboot/petclinic/model/Pet.java
@@ -1,8 +1,15 @@
 package guru.springboot.petclinic.model;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.time.LocalDate;
 
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name="pet")
 public class Pet extends BaseEntity{
@@ -21,35 +28,4 @@ public class Pet extends BaseEntity{
     @Column(name="name")
     private String name;
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public PetType getPetType() {
-        return petType;
-    }
-
-    public void setPetType(PetType petType) {
-        this.petType = petType;
-    }
-
-    public Owner getOwner() {
-        return owner;
-    }
-
-    public void setOwner(Owner owner) {
-        this.owner = owner;
-    }
-
-    public LocalDate getBirthDate() {
-        return birthDate;
-    }
-
-    public void setBirthDate(LocalDate birthDate) {
-        this.birthDate = birthDate;
-    }
 }
--- pet-clinic-data/src/main/java/guru/springboot/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springboot/petclinic/model/Vet.java
@@ -1,9 +1,14 @@
 package guru.springboot.petclinic.model;
 
-import javax.persistence.*;
-import java.util.HashSet;
-import java.util.Set;
+import lombok.*;
 
+import javax.persistence.*;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name="vets")
 public class Vet extends Person {
@@ -11,13 +16,4 @@ public class Vet extends Person {
     @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "vet_specialties", joinColumns =  @JoinColumn(name="vet_id"),
     inverseJoinColumns = @JoinColumn(name="specialty-id"))
-    private Set<Specialty> specialties = new HashSet<>();
-
-    public Set<Specialty> getSpecialties() {
-        return specialties;
-    }
-
-    public void setSpecialties(Set<Specialty> specialties) {
-        this.specialties = specialties;
-    }
 }
